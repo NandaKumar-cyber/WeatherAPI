5187488014c135d5f5444094098a8ff29461d600
Nanda Kumar <65768970+NandaKumar-cyber@users.noreply.github.com>
2023-02-21 18:53:19 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
 function App() {
+
+  const apiKey = process.env.REACT_APP_WEATHER_API_KEY
+  const [weatherData, setWeatherData] = useState([{}])
+  const [city, setCity] = useState("")
+
+  const getWeather = (event) => {
+    if (event.key) {
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${apiKey}`).then(
+        response => response.json()
+      ).then(
+        data => {
+          setWeatherData(data)
+        }
+      )
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <input
+        className='input'
+        placeholder='Enter city name'
+        onChange={e => setCity(e.target.value)}
+        value={city}
+        onKeyDown={getWeather} />
+
+      {typeof weatherData.main === 'undefined' ? (
+        <div>
+          <p className='search'>Search for City name</p>
+        </div>
+      ) : (
+        <div className='weather-data'>
+          <p className='cityname'>{weatherData.name}</p>
+          <p className='temp'>{Math.round(weatherData.main.temp)}°F</p>
+          <p className='weatherData'>{weatherData.weather[0].description}</p>
+        </div>
+      )}
+
+      {weatherData.cod === "404" ?(
+        <p>City not Found</p>
+      ):(
+        <></>
+      )}
+      
+      
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
